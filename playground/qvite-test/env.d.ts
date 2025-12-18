6be07cca9e4fcd3add68dfc5cf5aebe6b77eedfa
Quiteer <p602985@yeah.net>
2025-12-18 14:50:42 +0800

    refactor(other): 删除自动生成的类型文件跟踪

--- playground/qvite-test/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_BASE_URL: 'http://localhost:3000' | 'https://api.example.com' | 'https://api.release.example.com' | 'https://api.staging.example.com' | 'https://api.test.example.com'
-  readonly VITE_DESC: '发布环境变量' | '开发环境变量' | '测试环境变量' | '生产环境变量' | '通用环境变量' | '预发布环境变量'
-  readonly VITE_TEST_URL: 'https://quiteerjs.github.io/web/'
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
